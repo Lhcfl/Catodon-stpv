dc191d0f7b1ca3d6da442c83c1c7adb3d1c8db2b
Lhcfl <Lhcfl@outlook.com>
2024-01-03 03:14:26 +0800

    fix: #73 Weird dark background on inline code in chat view

--- packages/client/src/pages/messaging/messaging-room.message.vue
+++ packages/client/src/pages/messaging/messaging-room.message.vue
@@ -332,7 +332,11 @@ function del(): void {
 
 					> .text {
 						&,
-						::v-deep(*) {
+						::v-deep(a),
+						::v-deep(span),
+						::v-deep(p),
+						::v-deep(blockquote)
+						 {
 							color: var(--fgOnAccent) !important;
 						}
 					}
